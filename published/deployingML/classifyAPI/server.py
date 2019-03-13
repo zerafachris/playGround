43a97e10a60bb9e5d8a010622b7124cb99998c54
Unknown <zerafachris@gmail.com>
2019-03-13 22:54:41 +0100

    update deployML
    tweaked pipeline as it was throwing an error

--- published/deployingML/classifyAPI/server.py
+++ published/deployingML/classifyAPI/server.py
@@ -1,6 +1,7 @@
 #import os
 import pandas as pd
-import dill as pickle
+import dill
+import gzip
 from flask import Flask, jsonify, request
 
 
@@ -20,12 +21,16 @@ class ColumnSelector(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return X[self.columns]
 
-class ModifiedLabelEncoder(LabelEncoder):
-    def fit_transform(self, y, *args, **kwargs):
-        return super().fit_transform(y).reshape(-1, 1)
+class ModifiedLabelEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        return self
 
-    def transform(self, y, *args, **kwargs):
-        return super().transform(y).reshape(-1, 1)
+    def transform(self, X):
+        le = LabelEncoder().fit(X)
+        return le.transform(X).reshape(-1,1)
 
 # =============================================================================
 # Setting up the App
@@ -34,47 +39,42 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def apicall():
-	"""API Call"""
-	try:
-		test_json = request.get_json()
-		test = pd.read_json(test_json, orient='records')
+    try:
+        test_json = request.get_json()
+        test = pd.read_json(test_json, orient='records')
 
-		#Getting the Loan_IDs separated out
-		loan_ids = test['Loan_ID']
+        #Getting the Loan_IDs separated out
+        loan_ids = test['Loan_ID']
 
-	except Exception as e:
-		raise e
+    except Exception as e:
+        raise e
 
-	clf = 'classify_model.pk'
+    clf = 'classify_model.pk'
 
-	if test.empty:
-		return(bad_request())
-	else:
-		#Load the saved model
-		print("Load the model:")
-		loaded_model = None
-		with open('./model/'+clf,'rb') as f:
-			loaded_model = pickle.load(f)
+    if test.empty:
+        return(bad_request())
+    else:
+        #Load the saved model
+        print("Load the model:")
+        loaded_model = None
+        with gzip.open('./model/'+clf ,'rb') as f:
+            loaded_model = dill.load(f)
 
-		print("Doing predictions:")
-		predictions = loaded_model.predict(test)
+        print("Doing predictions:")
+        predictions = loaded_model.predict(test)
+        prediction_series = list(pd.Series(predictions))
+        final_predictions = pd.DataFrame(list(zip(loan_ids, prediction_series)))
+        responses = jsonify(predictions=final_predictions.to_json(orient="records"))
+        responses.status_code = 200
 
-		prediction_series = list(pd.Series(predictions))
-
-		final_predictions = pd.DataFrame(list(zip(loan_ids, prediction_series)))
-
-		responses = jsonify(predictions=final_predictions.to_json(orient="records"))
-		responses.status_code = 200
-
-		return (responses)
+        return (responses)
 
 
 @app.errorhandler(400)
 def bad_request(error=None):
-	message = {
-			'status': 400,
-			'message': 'Bad Request: ' + request.url }
-	resp = jsonify(message)
-	resp.status_code = 400
+    message = {'status': 400,
+               'message': 'Bad Request: ' + request.url }
+    resp = jsonify(message)
+    resp.status_code = 400
 
-	return resp
+    return resp
